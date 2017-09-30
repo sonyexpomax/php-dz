0452d1f243f999e5fedcff8bb33e99167fdc71d7
max <sonyexpomax3@gmail.com>
2017-09-30 22:11:51 +0300

    Переделанная работа с учетом замечаний

--- dz8-comments/index.php
+++ dz8-comments/index.php
@@ -23,11 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $author = htmlspecialchars($_POST['author']);
     $comment = htmlspecialchars($_POST['comment']);
     $email = htmlspecialchars($_POST['email']);
-    //var_dump($_POST);
-    echo $email."-----------";
-
-    echo search_email($email,$comments);
-    if (search_email($email,$comments)=='false') {
+    //var_dump(search_email($email,$comments));
+    if (search_email($email,$comments)==false) {
         if (strlen($author) && strlen($comment) && strlen($email)) {
             $comments[] = [
                 'date' => date('H:i:s d.m.Y'),
@@ -48,28 +45,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 usort($comments, function ($a, $b) {
     return (isset($a['timestamp']) && $a['timestamp'] > $b['timestamp']) ? -1 : 1;
 });
-//var_dump($comments);
-
 
 function search_email($email, $comments) {
     foreach ($comments as $key => $val) {
         if ($val['email'] == $email) {
-            return 'true';
+            return true;
             break;
         }
     }
-   return 'false';
+   return false;
 }
-//}
 
 ?>
 <DOCTYPE html>
     <html>
     <head>
         <meta charset="utf-8">
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
         <title>Comments</title>
     </head>
     <body>
@@ -78,7 +70,6 @@ function search_email($email, $comments) {
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <h2>Поделитесь вашим мнением</h2>
-
                 <?if (!empty($errors)):?>
                     <div class="alert alert-danger">
                         <?=implode('<br>', $errors)?>
@@ -113,13 +104,23 @@ function search_email($email, $comments) {
                 //---------------------------------------------- Вывод комметариев-----------------------------
                 $comments_count=count($comments);
                 $str_count= ceil($comments_count/5);
-
-                if (isset($_GET['p']) && $_GET['p'] >0 ) {
-                    $page = (int)$_GET['p'];
-                    $j_start = $page * 5 - 5;
-                    $j_end = $page * 5 - 1;
-
+                if (isset($_GET['p']) && ($_GET['p'] < 1 || $_GET['p'] > $str_count) ) {
+                    header( 'Location:?p=1');
+                }
+                else {
+                    //var_dump($comments);
+                    if (isset($_GET['p']) && $_GET['p'] > 0) {
+                        $page = (int)$_GET['p'];
+                        $j_start = $page * 5 - 5;
+                        $j_end = $page * 5 - 1;
+                    } else {
+                        $j_start = 0;
+                        $j_end = 4;
+                    }
                     for ($j = $j_start; $j <= $j_end; $j++) {
+                        // Убираем нежелательные слова из полей
+                        $comments[$j]['author'] = str_ireplace($censored,'[censored]',$comments[$j]['author']);
+                        $comments[$j]['comment'] = str_ireplace($censored,'[censored]',$comments[$j]['comment']);
                         echo "
                         <div class=\"panel panel-success\">
                                 <div class=\"panel-heading\">
@@ -140,14 +141,14 @@ function search_email($email, $comments) {
                     // Вывод ссылок постраничной навигации
                     echo "<div class=\"pagination\">";
                     for ($i = 1; $i <= $str_count; $i++) {
-                        if ($page==$i){
+                        if ($page == $i) {
                             echo "<strong style='margin: 5px; text-decoration: none; color: red;font-weight: 600; font-size: larger'>{$i}</strong>";
-                        }
-                        else{
+                        } else {
                             echo "<a style='margin: 5px;' href=\"?p={$i}\">{$i}</a>";
                         }
                     }
                 }
+
 
 
                 ?>
