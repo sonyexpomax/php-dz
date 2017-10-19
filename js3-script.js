f7f95e888a153f1c8a7e2ddb9cb267da112edb1d
max <sonyexpomax3@gmail.com>
2017-10-19 22:48:30 +0300

    дз js3 commit 1

--- js3-script.js
+++ js3-script.js
@@ -1,3 +1,188 @@
-/**
- * Created by max on 16.10.17.
- */
+
+
+//------------------------  Задание 1 -----------------------
+console.log("Задание 1");
+var obj1 = { name: 'Rea', age: 24 };
+var obj2 = { name: "Val", size: 43, look:"nice"};
+
+objects = function (obj1,obj2) {
+    var objRes = obj1;
+    var key;
+    for (key in obj2) {
+        if (obj2.hasOwnProperty(key)) {
+            objRes[key] = obj2[key]
+        }
+    }
+    return objRes;
+}
+console.log(objects(obj1,obj2));
+
+//------------------------  Задание 2 -----------------------
+console.log("Задание 2");
+var sportsmen = [
+    {name:"Ivan2", age: 33},
+    {name:"Ivan1", age: 31},
+    {name:"Ivan3", age: 21},
+    {name:"Ivan4", age: 20},
+    {name:"Ivan5", age: 42},
+    {name:"Ivan6", age: 41},
+    {name:"Ivan7", age: 36},
+    {name:"Ivan8", age: 42},
+    {name:"Ivan9", age: 22},
+];
+function topOldest(menlist, limit) {
+    if(menlist.length >= limit) {
+        menlist.sort(function (a, b) {
+            return a.age < b.age;
+        });
+        var i = 0;
+        console.log("Имена " + limit + " самых старых спортсменов");
+        while (i < limit) {
+
+            console.log('' +
+                'Спортсмен ' + menlist[i].name + ' - ' + menlist[i].age);
+            i++;
+        }
+    }
+    else {
+        alert("limit не может принимать значение больше чем количество спортсменов!");
+    }
+}
+topOldest(sportsmen,3);
+
+//--------------------------- Задание 3 ---------------------
+console.log('Задание 3')
+var runner = {
+    name:'hert',
+    medals: {}
+}
+function give(obj, medalType, medalNum) {
+    if (medalType == 'gold' || medalType == 'silver' || medalType == 'bronze') {
+        if (medalNum % 1 == 0 && medalNum > 0) {
+            if (obj.medals[medalType] != undefined) {
+                obj.medals[medalType] += medalNum;
+            }
+            else {
+                obj.medals[medalType] = medalNum;
+            }
+        }
+        else {
+            console.log('Количество медалей может быть только целым положительным');
+        }
+    }
+    else{
+        console.log('Медали могут быть только gold, silver, bronze');
+    }
+}
+give(runner,'gold', 3);
+give(runner,'silver', 23);
+give(runner,'gold', 8);
+console.log(runner.medals);
+
+
+//--------------------------- Задание 4 ---------------------
+console.log('Задание 4')
+var existTimer = false;
+var spammer = {
+    startSpam: function (input){
+        this.arr.push(input);
+        (!existTimer) ? StartStopTimer() : "";
+    },
+    stopSpam: function (output) {
+        this.arr.splice(this.arr.indexOf(output),1);
+        StartStopTimer();
+    },
+    arr:[]
+};
+
+function StartStopTimer() {
+    var fnc = setInterval(function() {
+        if (spammer.arr.length == 0) {
+            existTimer = false;
+            clearInterval(fnc);
+        }
+        else{
+            existTimer = true;
+            listStr();
+        }
+
+    }, 1000);
+}
+
+function listStr() {
+    var ListStrings ='';
+    spammer.arr.forEach(function(item,i,arr){
+        var TimeNow = new Date();
+        ListStrings = ListStrings + '\r\n' + arr[i] + "  //  " +
+            TimeNow.getHours()+ ":" +
+            TimeNow.getMinutes() + ":" +
+            TimeNow.getSeconds();
+    });
+    console.log(ListStrings);
+}
+
+//--------------------------- Задание 5 ---------------------
+console.log('Задание 5')
+function beautify(str) {
+    var smiles = [':)', ';)', '(:', ':p', ':D', ':-*'];
+    var punctation = ['.', ',', '?', '!', ';'];
+    var TempStr = '';
+    var len = str.length;
+    console.log('Входная строка \n' + str);
+    //console.log(punctation.indexOf(";"));
+    for (var i = 0; i < len; i++){
+        if (punctation.indexOf(str[i]) != -1 || str[i] == ' '){
+            var smileCode = Math.floor(Math.random(smiles.length-1) * (smiles.length));
+            TempStr += smiles[smileCode] + str[i];
+            if(str[i+1] == ' '){
+                i++;
+                TempStr += ' ';
+            }
+        }
+        else {
+            var registrCode = Math.floor(Math.random() * 2);
+            registrCode == 1 ? TempStr += str[i].toUpperCase() : TempStr += str[i].toLowerCase();
+        }
+    }
+    console.log('Выходная строка \n' + TempStr);
+}
+beautify('Поддерживается. в следующих, режимах документов; стандартный режим?');
+
+
+//--------------------------- Задание 6 ---------------------
+console.log('Задание 6')
+//var txt = "a22";
+//var encode = window.btoa(txt);
+var password = "YTFiMmMz";
+console.log("Зашифрованный пароль: " + password);
+
+function bruteForce(password) {
+    var arr = ['a', 'b', 'c',0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var size = 6;
+    var res = [];
+    var answer = '';
+    perebor(arr,res,size);
+    function perebor(arr,res,SizeSmall) {
+        if (SizeSmall != 0) {
+            for (var i = 0; i < arr.length; i++) {
+                res[size-SizeSmall] = arr[i];
+                perebor(arr, res, SizeSmall-1);
+                if (SizeSmall == 1 && answer =='') {
+                    var variant = res.join('');
+                    var encodedVariant = btoa(variant);
+                    if ( encodedVariant == password){
+                        answer = "Расшифрованный пароль: " + variant;
+                    }
+                }
+            }
+        }
+    }
+    if (answer == ''){
+        return "Расшифровать пароль не удалось";
+    }
+    else {
+        return answer;
+    }
+}
+
+console.log(bruteForce(password));
