157e598f8cd403e310d1e417cd367f61b29c2953
Maxim Kulinich <sonyexpomax3@gmail.com>
2017-11-23 14:53:41 +0200

    переделал Chicken

--- js7/library.js
+++ js7/library.js
@@ -1,14 +1,14 @@
 
-String.prototype.reverse = function (string) {
-    return string.split('').reverse().join('')
+String.prototype.reverse = function(){
+    return this.split('').reverse().join('')
 }
 
-String.prototype.intrim = function (string) {
-    return string.trim().replace(/\s{2,}/g, ' ');
+String.prototype.intrim = function () {
+    return this.trim().replace(/\s{2,}/g, ' ');
 }
 
-String.prototype.isPalindrome = function (string) {
-    return string == reverse(string) ? true : false;
+String.prototype.isPalindrome = function () {
+    return this == this.reverse() ? true : false;
 }
 
 Array.prototype.even = function(callback){
@@ -23,14 +23,14 @@ Array.prototype.odd = function(callback){
     }
 }
 
-Array.prototype.shuffle = function (arr) {
+Array.prototype.shuffle = function () {
     var randIndex;
-    var len =arr.length;
+    var len = this.length;
     var arrTemp = [];
     for(var i = len; i > 0; i--){
         randIndex =  Math.floor(Math.random() * i) ;
-        arrTemp.push(arr[randIndex]) ;
-        arr.splice(randIndex, 1);
+        arrTemp.push(this[randIndex]) ;
+        this.splice(randIndex, 1);
     }
     return arrTemp;
 }
--- js7/script_11.js
+++ js7/script_11.js
@@ -1,3 +1,4 @@
+
 //---------------------- задание1 ------------------
 var pet = {
     age: 11,
@@ -7,13 +8,11 @@ var pet = {
     },
     sleep: function () {
         console.log('I can sleep')
-    }
-};
-
-var homeAnimal = {
+    },
     furType: 'A+',
     tailLength: '5'
 };
+
 //--------for Cat-------
 var Cat = function () {
     this.meow = function () {
@@ -22,7 +21,6 @@ var Cat = function () {
 }
 
 Cat.prototype = pet;
-Cat.prototype = homeAnimal;
 
 //--------for Dog-------
 
@@ -33,7 +31,6 @@ var Dog = function () {
 }
 
 Dog.prototype = pet;
-Dog.prototype = homeAnimal;
 
 myCat = new Cat;
 myDog = new Dog;
@@ -42,28 +39,73 @@ myDog = new Dog;
 //----------------задание 2 ------------------
 
 var male = {
-    crow : function() {
+    crow: function () {
         alert("Cock-A-Doodle-Doo!");
     }
-};
+}
 
 var female = {
-    produceEgg : function() {
+    produceEgg: function () {
         var egg = {type: null};
         return egg;
     }
-};
+}
 
-var Chicken = function(name, sex) {
-    this.name = name;
-    if(sex == "male") {
-        this.__proto__ = male;
-    } else {
-        this.__proto__ = female;
+function Chicken(name, sex) {
+
+    if(sex = 'male'){
+        this.prototype = male;
     }
-    this.getSex = function() {
+
+    if(sex = 'female'){
+        this.prototype = female;
+    }
+
+    this.name = name;
+
+    this.getSex = function () {
         return sex;
     };
-};
+}
 
-myChicken = new Chicken('Rew','female');
+var myChicken1 = new Chicken('qqq','male');
+var myChicken2 = new Chicken('qwqwqw','female');
+
+//----------------задание 3 ------------------
+
+
+String.prototype.reverse = function(){
+    return this.split('').reverse().join('')
+}
+
+String.prototype.intrim = function () {
+    return this.trim().replace(/\s{2,}/g, ' ');
+}
+
+String.prototype.isPalindrome = function () {
+    return this == this.reverse() ? true : false;
+}
+
+Array.prototype.even = function(callback){
+    for (var i = 0; i< this.length; i+=2){
+        callback(this[i], i, this);
+    }
+}
+
+Array.prototype.odd = function(callback){
+    for (var i = 1; i< this.length; i+=2){
+        callback(this[i], i, this);
+    }
+}
+
+Array.prototype.shuffle = function () {
+    var randIndex;
+    var len = this.length;
+    var arrTemp = [];
+    for(var i = len; i > 0; i--){
+        randIndex =  Math.floor(Math.random() * i) ;
+        arrTemp.push(this[randIndex]) ;
+        this.splice(randIndex, 1);
+    }
+    return arrTemp;
+}
