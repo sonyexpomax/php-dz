908d60409961775f3f8c12de6c6f7eb318114659
Maxim Kulinich <sonyexpomax3@gmail.com>
2017-12-08 15:52:39 +0200

    переделаны modal и parallax

--- js11-dom-events/modal.js
+++ js11-dom-events/modal.js
@@ -1,65 +1,50 @@
-var countOfNewWindows = 0;
+let countOfNewWindows = 0;
 
 /**
  *  Creating New window
  * @param text
  */
 function CreateNewWindow(text) {
-    var newWindow = document.createElement('div');
-    newWindow.id = 'modal-' + (countOfNewWindows + 1);
-    newWindow.className = 'modal';
 
-    var positionY = countOfNewWindows * 50;
-    var positionX = countOfNewWindows * 50;
-
-    newWindow.style.top = positionY + "px";
-    newWindow.style.left = positionX + "px";
-
-    document.body.appendChild(newWindow);
+    let newWindow = document.createElement('div');
+    addNewWindowToDom(newWindow,text);
     countOfNewWindows ++;
-
-    CreateNewWindowHeader(newWindow.id);
-    CreateNewWindowCloseTab(newWindow.id);
-    CreateNewWindowParagraph(newWindow.id);
-    CreateNewWindowKeyText(newWindow.id, text);
-
-    var newElement = document.body.querySelector('#' + newWindow.id);
-    var newElementHeader = document.body.querySelector('#' + newWindow.id).firstChild;
+    let newWindowHeader = document.body.querySelector('#' + newWindow.id).firstChild;
 
     //drag n drop
-    newElementHeader.onmousedown = function(e) {
+    newWindowHeader.onmousedown = function(e) {
 
-        var coords = getCoords(newElement);
-        var shiftX = e.pageX - coords.left;
-        var shiftY = e.pageY - coords.top;
+        let coords = getCoords(newWindow);
+        let shiftX = e.pageX - coords.left;
+        let shiftY = e.pageY - coords.top;
 
-        newElement.style.position = 'absolute';
-        document.body.appendChild(newElement);
+        newWindow.style.position = 'absolute';
+        document.body.appendChild(newWindow);
         moveAt(e);
 
-        newElement.style.zIndex = 1000; // над другими элементами
+        newWindow.style.zIndex = 1000; // над другими элементами
 
         function moveAt(e) {
-            newElement.style.left = e.pageX - shiftX + 'px';
-            newElement.style.top = e.pageY - shiftY + 'px';
+            newWindow.style.left = e.pageX - shiftX + 'px';
+            newWindow.style.top = e.pageY - shiftY + 'px';
         }
 
         document.onmousemove = function(e) {
             moveAt(e);
         };
 
-        newElement.onmouseup = function() {
+        newWindow.onmouseup = function() {
             document.onmousemove = null;
-            newElement.onmouseup = null;
+            newWindow.onmouseup = null;
         };
     };
 
-    newElement.ondragstart = function() {
+    newWindow.ondragstart = function() {
         return false;
     };
 
     function getCoords(elem) {
-        var box = elem.getBoundingClientRect();
+        let box = elem.getBoundingClientRect();
         return {
             top: box.top + pageYOffset,
             left: box.left + pageXOffset
@@ -67,47 +52,36 @@ function CreateNewWindow(text) {
     }
 }
 
-/**
- * Creating New window header
- * @param windowId
- */
-function CreateNewWindowHeader(windowId) {
-    var newWindowHeader = document.createElement('h2');
-    newWindowHeader.textContent = 'Attention';
-    document.body.querySelector('#' + windowId).appendChild(newWindowHeader);
-}
+function addNewWindowToDom(newWindow, text) {
 
-/**
- * Creating New window close tab
- * @param windowId
- */
-function CreateNewWindowCloseTab(windowId) {
-    var parentElement = document.body.querySelector('#' + windowId);
-    var newWindowCloseTab = document.createElement('a');
+    //create div.modal
+    newWindow.id = 'modal-' + (countOfNewWindows + 1);
+    newWindow.className = 'modal';
+    newWindow.style.top = (countOfNewWindows * 50) + "px";
+    newWindow.style.left = (countOfNewWindows * 50) + "px";
+    
+    //create header
+    let newWindowHeader = document.createElement('h2');
+    newWindowHeader.textContent = 'Attention';
+    newWindow.appendChild(newWindowHeader);
+
+    //create p
+    let NewWindowParagraph = document.createElement('p');
+    NewWindowParagraph.textContent = 'You have pressed:';
+    
+    //create b
+    let NewWindowText = document.createElement('b');
+    NewWindowText.textContent = " '" + text + "'";
+    NewWindowParagraph.appendChild(NewWindowText);
+    newWindow.appendChild(NewWindowParagraph);
+
+    //create closetab
+    let newWindowCloseTab = document.createElement('a');
     newWindowCloseTab.className = 'close';
     newWindowCloseTab.onclick = function () {document.body.removeChild(parentElement);};
-    parentElement.appendChild(newWindowCloseTab);
-}
+    newWindow.appendChild(newWindowCloseTab);
 
-/**
- * Creating New window paragraph
- * @param windowId
- */
-function CreateNewWindowParagraph(windowId) {
-    var NewWindowParagraph = document.createElement('p');
-    NewWindowParagraph.textContent = 'You have pressed:';
-    document.body.querySelector('#' + windowId).appendChild(NewWindowParagraph);
-}
-
-/**
- * Creating New window paragraph text
- * @param windowId
- * @param text
- */
-function CreateNewWindowKeyText(windowId , text) {
-    var NewWindowText = document.createElement('b');
-    NewWindowText.textContent = " '" + text + "'";
-    document.body.querySelector('#' + windowId).lastChild.appendChild(NewWindowText);
+    document.body.appendChild(newWindow);
 }
 
 /**
@@ -115,7 +89,7 @@ function CreateNewWindowKeyText(windowId , text) {
  */
 document.body.addEventListener('keydown',function (event) {
     //console.log(event.keyCode);
-    var windowText = event.key;
+    let windowText = event.key;
     //    console.log(event.key);
     CreateNewWindow(windowText);
 });
