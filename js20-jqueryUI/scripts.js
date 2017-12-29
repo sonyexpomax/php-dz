34eaa4da819ba176c447dba87ea9470f912b6a49
Maxim Kulinich <sonyexpomax3@gmail.com>
2017-12-29 09:49:09 +0200

    fix multiple autocomplete

--- js20-jqueryUI/scripts.js
+++ js20-jqueryUI/scripts.js
@@ -26,9 +26,43 @@ var availableTags = [
     "Scheme"
 ];
 
+function split( val ) {
+    return val.split( /,\s*/ );
+}
+function extractLast( term ) {
+    return split( term ).pop();
+}
+
+$( "#autocomplete" ).on( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+            event.preventDefault();
+        }
+    });
 $( "#autocomplete" ).autocomplete({
-    source: availableTags
-});
+        minLength: 0,
+        source: function( request, response ) {
+            // delegate back to autocomplete, but extract the last term
+            response( $.ui.autocomplete.filter(
+                availableTags, extractLast( request.term ) ) );
+        },
+        focus: function() {
+            // prevent value inserted on focus
+            return false;
+        },
+        select: function( event, ui ) {
+            var terms = split( this.value );
+            console.log(this.value);
+            // remove the current input
+            terms.pop();
+            // add the selected item
+            terms.push( ui.item.value );
+            // add placeholder to get the comma-and-space at the end
+            terms.push( "" );
+            this.value = terms.join( ", " );
+            return false;
+        }
+    });
 
 $( ".controlgroup" ).controlgroup();
 $( ".controlgroup-vertical" ).controlgroup({
